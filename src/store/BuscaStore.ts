c3bf71c885920f3e7b08f30b707b456202170425
Rottschaefer <rottschaefer54@gmail.com>
2026-06-29 18:32:32 -0300

    Adicionando paginacao, funciona só pra gupy a logica

--- src/store/BuscaStore.ts
+++ src/store/BuscaStore.ts
@@ -1,43 +1,55 @@
-import { create } from "zustand"
+import { create } from "zustand";
 
 interface BuscaStore {
-    nomeInput: string,
-    nomePesquisa: string,
-    localInput: string,
-    localPesquisa: string,
-    fonteInput: string,
-    fontePesquisa: string,
+  nomeInput: string;
+  nomePesquisa: string;
+  localInput: string;
+  localPesquisa: string;
+  fonteInput: string;
+  fontePesquisa: string;
+  paginaAtual: number;
 
-    setNomeInput: (novoNomeInput: string) => void,
-    setNomePesquisa: (novoNomePesquisa: string) => void,
-    setLocalInput: (novoLocalInput: string) => void,
-    setLocalPesquisa: (novoLocalPesquisa: string) => void,
-    setFonteInput: (novoFonteInput: string) => void,
-    setFontePesquisa: (novoFontePesquisa: string) => void,
+  setNomeInput: (novoNomeInput: string) => void;
+  setNomePesquisa: (novoNomePesquisa: string) => void;
+  setLocalInput: (novoLocalInput: string) => void;
+  setLocalPesquisa: (novoLocalPesquisa: string) => void;
+  setFonteInput: (novoFonteInput: string) => void;
+  setFontePesquisa: (novoFontePesquisa: string) => void;
+  setPaginaAtual: (novaPaginaAtual: number) => void;
 
-    buscar: () => void,
+  buscar: () => void;
 }
 
 const useBuscaStore = create<BuscaStore>((set, get) => ({
-    nomeInput: "",
-    nomePesquisa: "",
-    localInput: "",
-    localPesquisa: "",
-    fonteInput: "Glassdoor",
-    fontePesquisa: "Glassdoor",
+  nomeInput: "",
+  nomePesquisa: "",
+  localInput: "",
+  localPesquisa: "",
+  fonteInput: "Gupy",
+  fontePesquisa: "Gupy",
+  paginaAtual: 1,
 
-    setNomeInput: (novoNomeInput: string) => set(() => ({nomeInput: novoNomeInput})),
-    setNomePesquisa: (novoNomePesquisa: string) => set(() => ({nomePesquisa: novoNomePesquisa})),
-    setLocalInput: (novoLocalInput: string) => set(() => ({localInput: novoLocalInput})),
-    setLocalPesquisa: (novoLocalPesquisa: string) => set(() => ({localPesquisa: novoLocalPesquisa})),
-    setFonteInput: (novoFonteInput: string) => set(() => ({fonteInput: novoFonteInput})),
-    setFontePesquisa: (novoFontePesquisa: string) => set(() => ({fontePesquisa: novoFontePesquisa})),
-
-    buscar: () => set({
-        nomePesquisa: get().nomeInput,
-        localPesquisa: get().localInput,
-        fontePesquisa: get().fonteInput,
+  setNomeInput: (novoNomeInput: string) =>
+    set(() => ({ nomeInput: novoNomeInput })),
+  setNomePesquisa: (novoNomePesquisa: string) =>
+    set(() => ({ nomePesquisa: novoNomePesquisa })),
+  setLocalInput: (novoLocalInput: string) =>
+    set(() => ({ localInput: novoLocalInput })),
+  setLocalPesquisa: (novoLocalPesquisa: string) =>
+    set(() => ({ localPesquisa: novoLocalPesquisa })),
+  setFonteInput: (novoFonteInput: string) =>
+    set(() => ({ fonteInput: novoFonteInput })),
+  setFontePesquisa: (novoFontePesquisa: string) =>
+    set(() => ({ fontePesquisa: novoFontePesquisa })),
+  setPaginaAtual: (novaPaginaAtual: number) =>
+    set(() => ({ paginaAtual: novaPaginaAtual })),
+  buscar: () =>
+    set({
+      nomePesquisa: get().nomeInput,
+      localPesquisa: get().localInput,
+      fontePesquisa: get().fonteInput,
+      paginaAtual: 1,
     }),
 }));
 
-export default useBuscaStore
+export default useBuscaStore;
